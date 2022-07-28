231fc74152faab24e270c6d5fbaac82884bd7663
Aman Bagrecha <bagrecha.aman588@gmail.com>
2022-07-28 20:43:08 +0530

    backup from harddrive fail

--- js/wowchemy-slides.js
+++ js/wowchemy-slides.js
@@ -2,7 +2,7 @@
   // ns-params:@params
   var slides = {highlight_style: "dracula", theme: "black"};
 
-  // ns-hugo:D:\notebooks\youtube_learn\create_github_website\blogdown\my_blog\themes\github.com\wowchemy\wowchemy-hugo-modules\wowchemy\assets\js\wowchemy-utils.js
+  // ns-hugo:D:\projects\my_website\themes\github.com\wowchemy\wowchemy-hugo-modules\wowchemy\assets\js\wowchemy-utils.js
   function fixMermaid() {
     let mermaids = [];
     [].push.apply(mermaids, document.getElementsByClassName("language-mermaid"));
